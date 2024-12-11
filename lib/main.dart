2c05b3311727684d73d03e88d0d20ba8a62542da
Javeria Noor <90166738+JiaNoor@users.noreply.github.com>
2024-12-12 00:31:57 +0500

    Add header

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:betatech/modules/home.dart';
+import 'package:betatech/modules/portfolio.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +14,8 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       initialRoute: '/',
       routes: {
-        '/': (context) => const HomePage(),
+        '/': (context) => Portfolio(),
+        //'/': (context) => const HomePage(),
         // '/anotherPage': (context) => const AnotherPage(),
       },
     );
